a897951f4e2f7bde38ad15a26a4b9e76d6074526
João Antonio Santana <joaoantoniosantana@outlook.com.br>
2020-09-17 17:25:07 -0300

    test: adiciona projeto de testes nUnit
    Resoeita a nomenclatura do nUnit

---
+++ src/ValidaCNPJCoreTests/ValidarTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using ValidaCNPJCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidaCNPJCore.Tests
+{
+    [TestFixture()]
+    public class ValidarTests
+    {
+        [Test()]
+        public void CNPJTest()
+        {
+            Assert.That(Validar.CNPJ(Convert.ToString(37048599000143)), Is.True);
+            Assert.That(Validar.CNPJ(Convert.ToString(37043599000145)), Is.False);
+        }
+    }
+}
